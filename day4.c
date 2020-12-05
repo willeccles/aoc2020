baa2c0991669a64778a56e8cc66ceee00851a2e4
Will Eccles <will@eccles.dev>
2020-12-04 20:16:25 -0500

    day 4 part 2 almost done

--- day4.c
+++ day4.c
@@ -1,60 +1,110 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
 
-#define BIT(n) (1 << n)
-
-#if 0
-#define BYR BIT(0)
-#define IYR BIT(1)
-#define EYR BIT(2)
-#define HGT BIT(3)
-#define HCL BIT(4)
-#define ECL BIT(5)
-#define PID BIT(6)
-#define CID BIT(7)
-#endif
-
-#define IS_VALID(mask) (((mask) & 0x7F) == 0x7F)
-
-struct passpart {
-    const char* name;
-    int value;
-} parts[] = {
-    { "byr", BIT(0) /*BYR*/ },
-    { "iyr", BIT(1) /*IYR*/ },
-    { "eyr", BIT(2) /*EYR*/ },
-    { "hgt", BIT(3) /*HGT*/ },
-    { "hcl", BIT(4) /*HCL*/ },
-    { "ecl", BIT(5) /*ECL*/ },
-    { "pid", BIT(6) /*PID*/ },
-    { "cid", BIT(7) /*CID*/ },
+static const char* eyecolors[] = {
+    "amb", "blu", "brn", "gry", "grn", "hzl", "oth"
 };
+
+int validate(const char* field, const char* value) {
+    static bool p = false;
+    if ((p = !p)) printf("validate(\"%s\", \"%s\")\n", field, value);
+    char* ivalsuffix;
+    long ival = strtol(value, &ivalsuffix, 10);
+    size_t vlen = strlen(value);
+    if (p) printf("ivalsuffix: %s\nival: %ld\nvlen: %zu\n", ivalsuffix, ival, vlen);
+
+    int rval;
+
+    if (!strcmp(field, "byr")) {
+        rval = vlen == 4 && (ival >= 1920 && ival <= 2002);
+    } else if (!strcmp(field, "iyr")) {
+        rval = vlen == 4 && (ival >= 2010 && ival <= 2020);
+    } else if (!strcmp(field, "eyr")) {
+        rval = vlen == 4 && (ival >= 2020 && ival <= 2030);
+    } else if (!strcmp(field, "hgt")) {
+        if (ivalsuffix == value) {
+            rval = 0;
+        } else if (!strcmp(ivalsuffix, "cm")) {
+            rval = ival >= 150 && ival <= 193;
+        } else if (!strcmp(ivalsuffix, "in")) {
+            rval = ival >= 59 && ival <= 76;
+        } else {
+            rval = 0;
+        }
+    } else if (!strcmp(field, "hcl")) {
+        if (strlen(value) != 7 || *value != '#') {
+            rval = 0;
+        } else {
+            for (int i = 1; i <= 6; i++) {
+                if (NULL == strchr("0123456789abcdef", value[i])) {
+                    rval = 0;
+                }
+            }
+            rval = 1;
+        }
+    } else if (!strcmp(field, "ecl")) {
+        for (int i = 0; i < 7; i++) {
+            if (!strcmp(value, eyecolors[i])) {
+                rval = 0;
+            }
+        }
+        rval = 0;
+    } else if (!strcmp(field, "pid")) {
+        if (strlen(value) != 9) {
+            rval = 0;
+        } else {
+            for (int i = 0; i < 9; i++) {
+                if (!isdigit(value[i])) {
+                    rval = 0;
+                }
+            }
+            rval = 1;
+        }
+    } else {
+        rval = -1;
+    }
+
+    printf("valid? %d\n", rval);
+    return rval;
+}
 
 int main(void) {
     char* line = NULL;
     size_t len = 0;
     ssize_t r = 0;
     int currpass1 = 0; // part 1
-    //int currpass2 = 0; // part 2
-    int valid = 0;
+    int currpass2 = 0; // part 2
+    int valid1 = 0;
+    int valid2 = 0;
+    char field[4] = {0};
+    char value[16] = {0};
 
     while ((r = getline(&line, &len, stdin)) > 0) {
         if (*line != '\n') {
-            for (int i = 0; i < 8; i++) {
-                if (NULL != strstr(line, parts[i].name)) {
-                    currpass1 |= parts[i].value;
-                }
+            char* tok = strtok(line, " \n");
+            while (tok) {
+                sscanf(tok, "%3s:%15s", field, value);
+                currpass1 += (validate(field, value) >= 0);
+                currpass2 += (validate(field, value) == 1);
+                tok = strtok(NULL, " \n");
             }
         } else {
-            valid += IS_VALID(currpass1);
+            valid1 += (currpass1 == 7);
+            valid2 += (currpass2 == 7);
             currpass1 = 0;
+            currpass2 = 0;
         }
     }
 
-    valid += IS_VALID(currpass1);
+    valid1 += (currpass1 == 7);
+    valid2 += (currpass2 == 7);
 
-    printf("Part 1: %d\n", valid);
+    printf("Part 1: %d\n", valid1);
+    printf("Part 2: %d\n", valid2);
 
     free(line);
 
